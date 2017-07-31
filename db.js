7f3c0b068d6b642a240dc1e03602b75661efc587
Burcu Noyan <burcunoyan@gmail.com>
2017-07-31 11:07:31 -0400

    set up seed, sync, createUser and create users table

--- db.js
+++ db.js
@@ -1,9 +1,64 @@
 // To do:
-// sync(cb),
-// seed(cb),
+// sync(cb),  OK
+// seed(cb),  OK
 // getUsers(managersOnly, cb)
 // getUser(id, cb)
-// createUser(user, cb)
+// createUser(user, cb) OK
 // updateUser(user, cb)
 // deleteUser(id, cb)
 
+var pg = require('pg');
+var client = new pg.Client(process.env.DATABASE_URL);
+
+client.connect(function(err) {
+  if (err) console.log(err.message);
+});
+
+function sync(cb) {
+  var sql = `
+    DROP TABLE IF EXISTS users;
+
+    CREATE TABLE users(
+      id SERIAL PRIMARY KEY,
+      name CHARACTER VARYING(255) UNIQUE NOT NULL,
+      manager BOOLEAN
+    );
+  `;
+
+  client.query(sql, null, function(err) {
+    if (err) return cb(err);
+    cb(null);
+  });
+}
+
+function seed(cb) {
+  createUser({ name: 'Jake the Dog', manager: 'false' }, function(err) {
+    if (err) return cb(err);
+    createUser({ name: 'Finn the Human', manager: 'false' }, function(err) {
+      if (err) return cb(err);
+      createUser({ name: 'Princess Bubblegum', manager: 'true' }, function(err) {
+       if (err) return cb(err);
+      });
+    });
+  });
+}
+
+function createUser(user, cb) {
+  var sql = `
+    INSERT INTO users(name, manager)
+    VALUES ($1, $2)
+    RETURNING id
+  `;
+
+  client.query(sql, [ user.name, user.manager ], function(err, result) {
+    if (err) cb(err);
+    cb(null, result.rows[0].id);
+  });
+}
+
+
+module.exports = {
+  sync,
+  seed,
+  createUser
+};
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const db = require('./db');
 
 const app = express();
 
@@ -24,4 +24,14 @@ app.use(function(err, req, res, next) {
 const port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log(`listening on port ${port}`);
+  db.sync(function(err) {
+    if (err) return console.log(err.message);
+    db.seed(function(err) {
+      if (err) return console.log(err.message);
+      db.getUsers(function(err) {
+        if (err) return console.log(err.message);
+        console.log(db.users);
+      });
+    });
+  });
 });
