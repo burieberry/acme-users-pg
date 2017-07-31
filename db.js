3461275fb505cb8688dda835ba0c07b8ed511a58
Burcu Noyan <burcunoyan@gmail.com>
2017-07-31 08:31:18 -0400

    init file structure

---
+++ db.js
@@ -0,0 +1,9 @@
+// To do:
+// sync(cb),
+// seed(cb),
+// getUsers(managersOnly, cb)
+// getUser(id, cb)
+// createUser(user, cb)
+// updateUser(user, cb)
+// deleteUser(id, cb)
+
---
+++ routes/users.js
@@ -0,0 +1,6 @@
+// To do:
+// GET '/users'
+// POST '/users'
+// PUT '/users/:id'
+// DELETE '/users/:id'
+// GET '/users/managers'
