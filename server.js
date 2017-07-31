5497144715d635d82d45f17dfbb2fcb2ed02043a
Burcu Noyan <burcunoyan@gmail.com>
2017-07-31 09:20:17 -0400

    start server

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+
+const app = express();
+
+// logging
+app.use(function(req, res, next) {
+  res.on('finish', function() {
+    console.log(req.method, req.url, res.statusCode);
+  });
+  next();
+});
+
+
+app.get('/', function(req, res, next) {
+  res.send('hello');
+});
+
+app.use(function(err, req, res, next) {
+  app.render('error', { error: err });
+});
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log(`listening on port ${port}`);
+});
